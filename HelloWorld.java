498f92e84a0ad41d0acc02d843b916eddcbdfc38
sabithaakula <67954034+sabithaakula@users.noreply.github.com>
2020-07-07 06:12:31 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 public static void main(String args[]){
-System.out.println("Hello world***************8");
+System.out.println("Hello world***************poll scm");
 }
 }
